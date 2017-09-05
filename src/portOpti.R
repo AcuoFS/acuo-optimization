ee992e00ffb28d6432b23271c5ca030ea66cea90
yuansilin <silin@acuo.com>
2017-09-05 15:22:20 +0800

    OW-806,update function I/O and test

--- src/portOpti.R
+++ src/portOpti.R
@@ -1,80 +1,83 @@
 
-portOpti <- function(accounts,tradesNum){
-  # accounts: DV01 of each account, initially 1 trade in each account
-  # N: number of accounts
-  # tradesNum: number of trades in each account
-  # accountsIdx: account index for trade
+portOpti <- function(DV01,tradesNum,portfoliosId){
+  # DV01: DV01 of each account, initially 1 trade in each portfolio(account)
+  # N: number of DV01
+  # tradesNum: number of trades in each portfolio(account)
+  # portfoliosId: portfolio Id for trades
+  # DV01Idx: portfolio index for trades
   # movements: record the movements number of the trades
   
-  N <- length(accounts)
-  accountsIdx <- 1:N
+  N <- length(DV01)
+  DV01Idx <- 1:N
   movements <- 0
   
-  accountsPos <- rep(0,N)
-  accountsNeg <- rep(0,N)
-  accountsPos[which(accounts>0)] <- accounts[which(accounts>0)]  
-  accountsNeg[which(accounts<0)] <- accounts[which(accounts<0)]
+  DV01Pos <- rep(0,N)
+  DV01Neg <- rep(0,N)
+  DV01Pos[which(DV01>0)] <- DV01[which(DV01>0)]  
+  DV01Neg[which(DV01<0)] <- DV01[which(DV01<0)]
 
-  while(sum(accountsPos)>0 && sum(accountsNeg)<0){
-    maxIdxPos <- which.max(accountsPos) # the maximum DV01 account index
-    maxPos <- accountsPos[maxIdxPos]
-    maxIdxNeg <- which.max(abs(accountsNeg))
-    maxNeg <- accountsNeg[maxIdxNeg]
+  while(sum(DV01Pos)>0 && sum(DV01Neg)<0){
+    maxIdxPos <- which.max(DV01Pos) # the maximum DV01 account index
+    maxPos <- DV01Pos[maxIdxPos]
+    maxIdxNeg <- which.max(abs(DV01Neg))
+    maxNeg <- DV01Neg[maxIdxNeg]
     
     # compare the trades in the two opposite account
     # move the smaller absolute DV01 to the larger DV01 account
     # if same, move the account with less trades
     if(maxPos > abs(maxNeg)){
-      accountsPos[maxIdxPos] <- maxPos + maxNeg
-      accountsNeg[maxIdxNeg] <- 0
+      DV01Pos[maxIdxPos] <- maxPos + maxNeg
+      DV01Neg[maxIdxNeg] <- 0
       movements <- movements + tradesNum[maxIdxNeg]
       tradesNum[maxIdxPos] <- tradesNum[maxIdxPos]+tradesNum[maxIdxNeg]
       tradesNum[maxIdxNeg] <- 0
-      accountsIdx[maxIdxNeg] <- maxIdxPos
+      DV01Idx[maxIdxNeg] <- maxIdxPos
       
       
     } else if(maxPos < abs(maxNeg)){
-      accountsPos[maxIdxPos] <- 0
-      accountsNeg[maxIdxNeg] <- maxPos + maxNeg
+      DV01Pos[maxIdxPos] <- 0
+      DV01Neg[maxIdxNeg] <- maxPos + maxNeg
       movements <- movements + tradesNum[maxIdxPos]
       tradesNum[maxIdxPos] <- 0
       tradesNum[maxIdxNeg] <- tradesNum[maxIdxNeg]+tradesNum[maxIdxPos]
-      accountsIdx[maxIdxPos] <- maxIdxNeg
+      DV01Idx[maxIdxPos] <- maxIdxNeg
       
       
     } else{
       if(tradesNum[maxIdxPos] > tradesNum[maxIdxNeg]){
-        accountsPos[maxIdxPos] <- maxPos + maxNeg
-        accountsNeg[maxIdxNeg] <- 0
+        DV01Pos[maxIdxPos] <- maxPos + maxNeg
+        DV01Neg[maxIdxNeg] <- 0
         movements <- movements + tradesNum[maxIdxNeg]
         tradesNum[maxIdxPos] <- tradesNum[maxIdxPos]+tradesNum[maxIdxNeg]
         tradesNum[maxIdxNeg] <- 0
-        accountsIdx[maxIdxNeg] <- maxIdxPos
+        DV01Idx[maxIdxNeg] <- maxIdxPos
         
       } else{
-        accountsPos[maxIdxPos] <- 0
-        accountsNeg[maxIdxNeg] <- maxPos + maxNeg
+        DV01Pos[maxIdxPos] <- 0
+        DV01Neg[maxIdxNeg] <- maxPos + maxNeg
         movements <- movements + tradesNum[maxIdxPos]
         tradesNum[maxIdxPos] <- 0
         tradesNum[maxIdxNeg] <- tradesNum[maxIdxNeg]+tradesNum[maxIdxPos]
-        accountsIdx[maxIdxPos] <- maxIdxNeg
+        DV01Idx[maxIdxPos] <- maxIdxNeg
         
       }
     }
-    accounts <- accountsPos+accountsNeg
+    DV01 <- DV01Pos+DV01Neg
   }
-  cat("DV01 of each account:",accounts,'\n')
-  cat("Number of trades in each account",tradesNum,'\n')
-  cat("Original account of each trade",accountsIdx,'\n')
-  cat("Total movements:",movements,'\n')
+  portfoliosId <- portfoliosId[DV01Idx]
+  
+  #cat("DV01 of each account:",DV01,'\n')
+  #cat("Number of trades in each account",tradesNum,'\n')
+  #cat("Original account of each trade",DV01Idx,'\n')
+  #cat("Total movements:",movements,'\n')
   
   # instructions
   for(k in 1:N){
-    if(accountsIdx[k]!=k){
-      cat('Move trade(s) from account [', k, '] to account [', accountsIdx[k],'].','\n')
+    if(DV01Idx[k]!=k){
+      #cat('Move trade(s) from portfolio [', k, '] to portfolio [', portfoliosId[k],'].','\n')
     }
   }
-  result <- list(accounts=accounts,tradesNum=tradesNum,accountsIdx=accountsIdx)
+  result <- list(DV01=DV01,tradesNum=tradesNum,portfoliosId=portfoliosId,movements=movements)
 }
 
 
--- test/testPortOpti/portOptiTests.R
+++ test/testPortOpti/portOptiTests.R
@@ -1,15 +1,18 @@
 source('src/portOpti.R')
 
-accounts1 <- c(80,-50,-20,40,70,-70,50,-70)
+DV01_1 <- c(80,-50,-20,40,70,-70,50,-70)
 tradesNum1 <- c(1,2,4,1,3,1,2,1)
+portfoliosId1 <- c('p1','p2','p3','p4','p5','p6','p7','p8')
 
 
 testPortOptiWithMultiTradesInEachAccount <- function(){
-  accounts <- accounts1
+  DV01 <- DV01_1
   tradesNum <- tradesNum1
-  result <- portOpti(accounts,tradesNum)
+  portfoliosId <- portfoliosId1
   
-  checkEquals(result$accounts,c(10,0,0,20,0,0,0,0))
+  result <- portOpti(DV01,tradesNum,portfoliosId)
+  
+  checkEquals(result$DV01,c(10,0,0,20,0,0,0,0))
   checkEquals(result$tradesNum,c(2,2,0,5,4,0,0,0))
-  checkEquals(result$accountsIdx,c(1,2,4,4,5,1,2,5))
+  checkEquals(result$portfoliosId,c('p1','p2','p4','p4','p5','p1','p2','p5'))
 }
